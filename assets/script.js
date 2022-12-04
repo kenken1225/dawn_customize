cd3e7a29b1adc284cf13a01808e987601cf30857
kenya <igot.never.1015@gmail.com>
2022-12-04 21:24:44 +0900

    12/4カスタムコレクション修正

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,36 @@
 
-	$('.slider-wrap').slick({
-		fade:true,//切り替えをフェードで行う。初期値はfalse。
-		autoplay: true,//自動的に動き出すか。初期値はfalse。
-		autoplaySpeed: 6000,//次のスライドに切り替わる待ち時間
-		speed:1500,//スライドの動きのスピード。初期値は300。
-		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		slidesToShow: 1,//スライドを画面に3枚見せる
-		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-		arrows: true,//左右の矢印あり
-		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-		dots: true,//下部ドットナビゲーションの表示
-        pauseOnFocus: false,//フォーカスで一時停止を無効
-        pauseOnHover: false,//マウスホバーで一時停止を無効
-        pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+$('.slider-wrap').slick({
+	fade: true,//切り替えをフェードで行う。初期値はfalse。
+	autoplay: true,//自動的に動き出すか。初期値はfalse。
+	autoplaySpeed: 6000,//次のスライドに切り替わる待ち時間
+	speed: 1500,//スライドの動きのスピード。初期値は300。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 1,//スライドを画面に3枚見せる
+	slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+	arrows: true,//左右の矢印あり
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	dots: true,//下部ドットナビゲーションの表示
+	pauseOnFocus: false,//フォーカスで一時停止を無効
+	pauseOnHover: false,//マウスホバーで一時停止を無効
+	pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
 });
 
 //スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
-$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-    $('.slider').slick('slickPlay');
+$('.slider').on('touchmove', function (event, slick, currentSlide, nextSlide) {
+	$('.slider').slick('slickPlay');
 });
 
 
 
 
 //任意のタブにURLからリンクするための設定
-function GethashID (hashIDName){
-	if(hashIDName){
+function GethashID(hashIDName) {
+	if (hashIDName) {
 		//タブ設定
-		$('.tab li').find('a').each(function() { //タブ内のaタグ全てを取得
+		$('.tab li').find('a').each(function () { //タブ内のaタグ全てを取得
 			var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得	
-			if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+			if (idName == hashIDName) { //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
 				var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
 				$('.tab li').removeClass("active"); //タブ内のliについているactiveクラスを取り除き
 				$(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
@@ -43,19 +43,19 @@ function GethashID (hashIDName){
 }
 
 //タブをクリックしたら
-$('.tab a').on('click', function() {
+$('.tab a').on('click', function () {
 	var idName = $(this).attr('href'); //タブ内のリンク名を取得	
-	GethashID (idName);//設定したタブの読み込みと
+	GethashID(idName);//設定したタブの読み込みと
 	return false;//aタグを無効にする
 });
 
 
 // 上記の動きをページが読み込まれたらすぐに動かす
 $(window).on('load', function () {
-    $('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
-    $('.special__contents:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
+	$('.tab li:first-of-type').addClass("active"); //最初のliにactiveクラスを追加
+	$('.special__contents:first-of-type').addClass("is-active"); //最初の.areaにis-activeクラスを追加
 	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
-	GethashID (hashName);//設定したタブの読み込み
+	GethashID(hashName);//設定したタブの読み込み
 });
 
 
@@ -77,50 +77,50 @@ $('.choice-btn').slick({
 	focusOnSelect: true, //フォーカスの有効化
 	asNavFor: '.gallery', //連動させるスライドショーのクラス名
 });
-  
+
 //下の選択画像をスライドさせずに連動して変更させる設定。
-$('.gallery').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+$('.gallery').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
 	var index = nextSlide; //次のスライド番号
 	//サムネイルのslick-currentを削除し次のスライド要素にslick-currentを追加
 	$(".choice-btn .slick-slide").removeClass("slick-current").eq(index).addClass("slick-current");
 });
-  
+
 
 
 function toggleNav() {
-  var body = document.body;
-  var hamburger = document.getElementById('js-hamburger');
-   var blackBg = document.getElementById('js-black-bg');
+	var body = document.body;
+	var hamburger = document.getElementById('js-hamburger');
+	var blackBg = document.getElementById('js-black-bg');
 
-  hamburger.addEventListener('click', function() {
-    body.classList.toggle('nav-open');
-  });
-  blackBg.addEventListener('click', function() {
-    body.classList.remove('nav-open');
-  });
+	hamburger.addEventListener('click', function () {
+		body.classList.toggle('nav-open');
+	});
+	blackBg.addEventListener('click', function () {
+		body.classList.remove('nav-open');
+	});
 }
 toggleNav();
 
 
 
 //ドロップダウンの設定を関数でまとめる
-function mediaQueriesWin(){
+function mediaQueriesWin() {
 	var width = $(window).width();
-		$(".has-child>a").on('click', function() {//has-childクラスがついたaタグをクリックしたら
-			var parentElem =  $(this).parent();// aタグから見た親要素のliを取得し
-			$(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
-			$(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
-			return false;//リンクの無効化
-		});
+	$(".has-child>a").on('click', function () {//has-childクラスがついたaタグをクリックしたら
+		var parentElem = $(this).parent();// aタグから見た親要素のliを取得し
+		$(parentElem).toggleClass('active');//矢印方向を変えるためのクラス名を付与して
+		$(parentElem).children('ul').stop().slideToggle(500);//liの子要素のスライドを開閉させる※数字が大きくなるほどゆっくり開く
+		return false;//リンクの無効化
+	});
 }
 
 // ページがリサイズされたら動かしたい場合の記述
-$(window).resize(function() {
+$(window).resize(function () {
 	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
 });
 
 // ページが読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load',function(){
+$(window).on('load', function () {
 	mediaQueriesWin();/* ドロップダウンの関数を呼ぶ*/
 });
 
@@ -130,14 +130,28 @@ $(window).on('load',function(){
 //サイドバーカートページに動きを追加
 $(".openbtn").click(function () {//ボタンがクリックされたら
 	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
-   $("#sidebar-cart").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
-   $("#js-black-bg").toggleClass('active');//ナビゲーションにactiveクラスを付与
-   
+	$("#sidebar-cart").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+	$("#js-black-bg").toggleClass('active');//ナビゲーションにactiveクラスを付与
+
 });
 
-$('#js-black-bg').click(function() {
- $(this).removeClass('active');
- $("#sidebar-cart").removeClass('panelactive');
+$('#js-black-bg').click(function () {
+	$(this).removeClass('active');
+	$("#sidebar-cart").removeClass('panelactive');
 
 })   //！！右側カートサイドバーを使うときの記載！！
+
+//数量カウンター
+$('.qtybox .btnqty').on('click', function () {
+	var qty = parseInt($(this).parent('.qtybox').find('.quantity-input').val());
+	if ($(this).hasClass('qtyplus')) {
+		qty++;
+	} else {
+		if (qty > 1) {
+			qty--;
+		}
+	}
+	qty = (isNaN(qty)) ? 1 : qty;
+	$(this).parent('.qtybox').find('.quantity-input').val(qty);
+});
 
